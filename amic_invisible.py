2081a2313bedc502bf980bff8305dc24881b950a
Joan Claverol <joanclaverol@Joans-MacBook-Pro.local>
2021-12-26 13:15:44 +0100

    formated output

--- amic_invisible.py
+++ amic_invisible.py
@@ -1,63 +1,66 @@
 from random import choice, shuffle
 
-class generador_amic_invisible(): 
 
-    def __init__(self, amics: list): 
+class generador_amic_invisible:
+    def __init__(self, amics: list):
         self.amics = amics
 
     def genera_flat_amics(self):
         self.flat_amics = []
         self.parelles = []
         self.solters = []
-        for amic in self.amics: 
-            if type(amic) is list: 
+        for amic in self.amics:
+            if type(amic) is list:
                 parella_seleccionada = []
-                for parella in amic: 
+                for parella in amic:
                     self.flat_amics.append(parella.lower())
                     parella_seleccionada.append(parella.lower())
                 self.parelles.append(parella_seleccionada)
-            else: 
+            else:
                 self.flat_amics.append(amic.lower())
                 self.solters.append(amic.lower())
         shuffle(self.flat_amics)
 
     def trobar_parella(self, persona):
-        persona = persona.lower() 
-        for parella in self.parelles: 
-            if persona in parella: 
+        persona = persona.lower()
+        for parella in self.parelles:
+            if persona in parella:
                 self.parella = [nom for nom in parella if nom != persona][0]
                 break
-            else: 
+            else:
                 self.parella = None
 
-    def genera_amics_invisibles(self): 
-        
+    def genera_amics_invisibles(self):
+
         self.genera_flat_amics()
         self.amics_invisibles = []
         resta_amic_inv = self.flat_amics.copy()
-        for amic in self.flat_amics: 
+        for amic in self.flat_amics:
             amic_seleccionat = amic
             self.trobar_parella(amic)
             counter = 0
             while amic_seleccionat == amic or amic_seleccionat == self.parella:
                 amic_seleccionat = choice(resta_amic_inv)
-                if counter > len(self.flat_amics) * 5: 
+                if counter > len(self.flat_amics) * 5:
                     self.genera_amics_invisibles()
                     break
                 counter += 1
             self.amics_invisibles.append([amic, amic_seleccionat])
             resta_amic_inv.remove(amic_seleccionat)
 
+
 amics = [
-    ['Jordi', 'MCarmen'],
-    'Yayo', 
-    'Cristina',
-    'Joel',
-    ['Alberto','Pili'],
-    ['Roger','Txell'],
-    ['Joan','Nuria']
+    ["Jordi", "MCarmen"],
+    "Yayo",
+    "Cristina",
+    "Joel",
+    ["Alberto", "Pili"],
+    ["Roger", "Txell"],
+    ["Joan", "Nuria"],
 ]
 
 c = generador_amic_invisible(amics)
 c.genera_amics_invisibles()
-print(c.amics_invisibles)
+[print(parella, sep='\n') for parella in c.amics_invisibles]
+
+
---
+++ nadal_2021.py
@@ -0,0 +1,13 @@
+[
+    ["cristina", "joel"],
+    ["jordi", "cristina"],
+    ["roger", "pili"],
+    ["txell", "jordi"],
+    ["joan", "alberto"],
+    ["pili", "nuria"],
+    ["yayo", "joan"],
+    ["alberto", "yayo"],
+    ["mcarmen", "txell"],
+    ["joel", "mcarmen"],
+    ["nuria", "roger"],
+]
